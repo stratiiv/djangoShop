37319f87649e7ec152953326780f83cc33ff6ed3
stratiiv <stratiyvalter@gmail.com>
2023-01-30 19:18:56 +0200

    register order admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Product,Category
+from .models import Product,Category,Order
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id','name','price','user','category')
     list_filter = ('category',)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id','name','email','product','get_price')
+
+    @admin.display
+    def get_price(self,obj):
+        return obj.product.price
 
 admin.site.register(Category)
