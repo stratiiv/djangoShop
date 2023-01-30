2dac3d7b89faaff0916816d93a37eaca53b4155a
stratiiv <stratiyvalter@gmail.com>
2023-01-30 18:42:05 +0200

    register models in admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+from .models import Product,Category
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id','name','price','user','category')
+    list_filter = ('category',)
+
+admin.site.register(Category)
