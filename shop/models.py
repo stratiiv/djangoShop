35cb1e4be58e49b110707ad391d63e405f909fac
stratiiv <stratiyvalter@gmail.com>
2023-01-30 18:41:12 +0200

    add models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Category(models.Model):
+    def __str__(self) -> str:
+        return self.name
+    name = models.CharField(max_length=100)
+
+class Product(models.Model):
+    def __str__(self) -> str:
+        return f'{self.name}({self.category})'
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+class Order(models.Model):
+    def __str__(self) -> str:
+        return f'Order #{self.id}'
+    name = models.CharField(max_length=50)
+    product = models.ForeignKey(Product)
+    email = models.EmailField()
+    
