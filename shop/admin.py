22d7db1da775aee4c3b5a434835ab0b9a00a3463
stratiiv <stratiyvalter@gmail.com>
2023-01-30 21:00:31 +0200

    add product admin searchfield

--- shop/admin.py
+++ shop/admin.py
@@ -5,7 +5,7 @@ from .models import Product,Category,Order
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id','name','price','user','category')
     list_filter = ('category',)
-
+    search_fields = ('id','name','price','user__username','category__name')
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id','customer','email','product','price','category')
