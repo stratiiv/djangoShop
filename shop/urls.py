4d429b009a05c2819fa7cc66d515ad9df257cce4
stratiiv <stratiyvalter@gmail.com>
2023-01-31 17:16:52 +0200

    implement ordering api

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('products/',views.get_product_list,name='products'),
     path('products/filter/',views.get_filtered_product_list,name='filter-products'),
     path('products/<int:pk>/',views.ProductDetailView.as_view(),name = 'product-detail'),
-    path('create-order/',views.create_order,name = 'create-order')
+    path('create-order/',views.create_order,name = 'create-order'),
+    path('api/v1/create-order/',views.CreateOrder.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- shop/views.py
+++ shop/views.py
@@ -4,6 +4,10 @@ from django.urls import reverse
 from django.views import generic
 from .models import Product
 from .forms import OrderForm
+from rest_framework.views import APIView
+from .serializers import OrderSerializer
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED,HTTP_400_BAD_REQUEST
 # Create your views here.
 
 class ProductFilter(django_filters.FilterSet):
@@ -34,4 +38,12 @@ def create_order(request):
     else:
         form = OrderForm()
         return render(request,'shop/create_order.html',{'form':form})
+
+class CreateOrder(APIView):
+    def post(self,request):
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,HTTP_201_CREATED)
+        return Response(serializer.errors,HTTP_400_BAD_REQUEST)
 
