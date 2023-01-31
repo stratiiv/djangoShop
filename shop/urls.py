aae37a9badbe6ebc97435315b598147feeab268d
stratiiv <stratiyvalter@gmail.com>
2023-01-31 12:53:05 +0200

    add product filtering

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('products/',views.get_product_list)
+    path('products/',views.get_product_list,name='products'),
+    path('products/filter',views.get_filtered_product_list,name='filter-products')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,20 @@
+import django_filters
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Product
 # Create your views here.
 
+class ProductFilter(django_filters.FilterSet):
+    class Meta:
+        model = Product
+        fields = ['name','price','category']
+
 def get_product_list(request):
+    f = ProductFilter(request.GET,queryset=Product.objects.all())
     queryset = Product.objects.all()
-    return render(request,'shop/product_list.html',{'product_list':queryset})
+    return render(request,'shop/product_list.html',{'product_list':queryset,'filter':f})
+
+
+def get_filtered_product_list(request):
+    f = ProductFilter(request.GET,queryset=Product.objects.all())
+    return render(request,'shop/filtered_products.html',{'filter':f})
