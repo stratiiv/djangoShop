13b79b267dd299e4058ee53d8637b8b8b02cb31c
stratiiv <stratiyvalter@gmail.com>
2023-01-31 08:36:42 +0200

    implement product list view

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def get_product_list(request):
+    queryset = Product.objects.all()
+    return render(request,'shop/product_list.html',{'product_list':queryset})
